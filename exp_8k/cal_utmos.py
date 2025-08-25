bff891c268cab7e8774ae0226205991039fa3991
22wjy <junyi-wa22@mails.tsinghua.edu.cn>
2025-08-25 20:25:22 +0800

    modified exp_8k

--- exp_8k/cal_utmos.py
+++ exp_8k/cal_utmos.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # 输入输出路径
 input_dir = "/mnt/nvme_share/srt30/checkpoint/exp_8k/output_wav_860k"
-output_csv = "/mnt/nvme_share/srt30/APCodec-AP-BWE-Reproduction/exp_8k/file.csv"
+output_csv = "/mnt/nvme_share/srt30/APCodec-Reproduction/exp_8k/file.csv"
 
 # 加载 SpeechMOS 模型
 predictor = torch.hub.load("tarepan/SpeechMOS:v1.2.0", "utmos22_strong", trust_repo=True)
